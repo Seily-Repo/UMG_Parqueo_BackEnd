dbe808bbf57afc6902bcc628ca0c7e5ba91f388d
mc151400-maker <chavitocastri@gmail.com>
2026-04-01 04:51:32 -0600

    Create planparqueo.controller.js
    CREAR PLAN

---
+++ src/controllers/planparqueo.controller.js
@@ -0,0 +1,38 @@
+async function crearPlan(req, res) {
+
+    let connection;
+
+    try {
+
+        connection = await getConnection();
+
+        const { PLA_ID_PLAN, PLA_DESCRIPCION, PLA_PRECIO, PLA_ESTADO } = req.body;
+
+        await connection.execute(
+            `
+            INSERT INTO PLAN_PARQUEO
+            (PLA_ID_PLAN, PLA_DESCRIPCION, PLA_PRECIO, PLA_ESTADO)
+            VALUES
+            (:id, :descripcion, :precio, :estado)
+            `,
+            {
+                id: PLA_ID_PLAN,
+                descripcion: PLA_DESCRIPCION,
+                precio: PLA_PRECIO,
+                estado: PLA_ESTADO
+            },
+            { autoCommit: true }
+        );
+
+        res.json({
+            mensaje: "Plan creado correctamente"
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+
+    }
+}
