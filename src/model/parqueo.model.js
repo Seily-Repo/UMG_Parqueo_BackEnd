9960f42b94179aee03f368523550b8e7f5779c3b
EberRivera <131565986+EberRivera@users.noreply.github.com>
2026-04-10 16:53:31 -0600

    fix : Response handler
    Up to date

--- src/model/parqueo.model.js
+++ src/model/parqueo.model.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const {sequelize} = require('../config/db');
+const { sequelize } = require('../config/db');
 
 const Parqueo = sequelize.define('Parqueo', {
     PQ_Parqueo: {
@@ -7,18 +7,55 @@ const Parqueo = sequelize.define('Parqueo', {
         primaryKey: true,
         autoIncrement: true
     },
+
     PQ_Nombre: {
-        type: DataTypes.STRING(100)
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'El nombre no puede estar vacío'
+            },
+            is: {
+                args: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/,
+                msg: 'El nombre solo debe contener letras y espacios'
+            }
+        }
     },
+
     PQ_Direccion: {
-        type: DataTypes.STRING(255)
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'La dirección no puede estar vacía'
+            },
+            is: {
+                args: /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s,#.-]+$/,
+                msg: 'La dirección contiene caracteres inválidos'
+            }
+        }
     },
+
     PQ_Capacidad: {
-        type: DataTypes.INTEGER 
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'La capacidad es obligatoria'
+            },
+            isInt: {
+                msg: 'La capacidad debe ser un número entero'
+            },
+            min: {
+                args: [1],
+                msg: 'La capacidad debe ser mayor a 0'
+            }
+        }
     }
+
 }, {
     tableName: 'DP_PARQUEO',
-    timestamps: false 
+    timestamps: false
 });
 
 module.exports = Parqueo;
