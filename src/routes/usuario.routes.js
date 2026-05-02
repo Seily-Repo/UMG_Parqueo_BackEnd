965041e8c4d85982d83260fa43719d0ada8f11ef
pablohub23 <pablodev23@gmail.com>
2026-05-01 21:08:25 -0600

    fix: usuario.routes.js

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -5,7 +5,5 @@ const usuarioController = require("../controllers/usuario.controller");
 // Rutas CRUD básicas
 router.get("/", usuarioController.getAllUsuarios);
 router.get("/carne/:carne", usuarioController.getUsuarioByCarne);
-router.post("/", usuarioController.createUsuario);
-router.put("/carne/:carne", usuarioController.updateUsuario);
 
 module.exports = router;
