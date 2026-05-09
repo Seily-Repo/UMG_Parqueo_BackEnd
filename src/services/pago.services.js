a8985f1131fefa718d637a0f324a0b5d8e9c14ea
snayersh <juanca1rlos1111@gmail.com>
2026-05-08 21:24:58 -0600

    feat: add authorization header to payment validation API call

--- src/services/pago.services.js
+++ src/services/pago.services.js
@@ -1,12 +1,16 @@
 const axios = require('axios'); 
 
 class PagoService {
-    static async validarPagoEnAPI(correlativo) {
+    static async validarPagoEnAPI(correlativo,authHeader) {
         try {
             
             const baseUrl = process.env.COBROS_API_BASE_URL || 'http://localhost:3000'; 
             const urlpago = `${baseUrl}/verify/${correlativo}`;
-            const respuesta = await axios.get(urlpago);
+            const respuesta = await axios.get(urlpago, {
+                headers: {
+                    'Authorization': authHeader
+                }
+            });
             return respuesta.data; 
             
 
