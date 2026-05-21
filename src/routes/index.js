39d405e1d4709e318b58077669d135b1273f18a8
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-05-20 19:15:08 -0600

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const reportesRoutes = require('./reportes.routes');
 
 const routes = (app) => {
-  app.use('', reportesRoutes);
+  app.use('/api/reportes', reportesRoutes);
 };
 
 module.exports = routes;
