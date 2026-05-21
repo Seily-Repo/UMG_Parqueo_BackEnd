23b0f5da20939aa020dfda36424a822de62c6992
Martin Tecun <tecunmartin132@gmail.com>
2026-05-21 17:35:24 -0600

    Se le coloca proteccion por roles a los endpoints de post y put

--- src/routes/isla.routes.js
+++ src/routes/isla.routes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const IslaController = require('../controllers/isla.controller');
 const { verifyToken, checkRole } = require('../middleware/auth.middleware');
 
-router.post('/', verifyToken, IslaController.createIsla);
+router.post('/', verifyToken, checkRole(['ADMINISTRADOR']), IslaController.createIsla);
 
 router.get('/', verifyToken, checkRole(['ADMINISTRADOR', 'USUARIO']), IslaController.getAllIslas);
 
 router.get('/:id/espacios', verifyToken, checkRole(['ADMINISTRADOR', 'USUARIO']), IslaController.getDetalleIsla);
 
-router.put('/:id/anular', verifyToken, IslaController.anularIsla);
+router.put('/:id/anular', verifyToken, checkRole(['ADMINISTRADOR']), IslaController.anularIsla);
 
 module.exports = router;
