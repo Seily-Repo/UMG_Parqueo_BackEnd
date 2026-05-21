b5fbbf9555a68091c7b7a66472e4262966ec1a3e
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-05-20 21:31:38 -0600

    middleware agregado

--- src/routes/reportes.routes.js
+++ src/routes/reportes.routes.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const reportesController = require('../controllers/reportes.controller');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 //financieros
-router.get('/reporte-financiero', reportesController.getReporteFinanciero);
-router.get('/pagos-aceptados', reportesController.getPagosAceptados);
+router.get('/reporte-financiero', authMiddleware.verifyToken, authMiddleware.checkRole(['ADMINISTRADOR']), reportesController.getReporteFinanciero);
+router.get('/pagos-aceptados', authMiddleware.verifyToken, authMiddleware.checkRole(['ADMINISTRADOR']), reportesController.getPagosAceptados);
 
 //administrativos
-router.get('/reporte-administrativo', reportesController.getReporteAdministrativo);
-router.get('/excel', reportesController.createExcel);
-router.get('/pdf', reportesController.createPDF);
+router.get('/reporte-administrativo', authMiddleware.verifyToken, authMiddleware.checkRole(['ADMINISTRADOR']), reportesController.getReporteAdministrativo);
+router.get('/excel', authMiddleware.verifyToken, authMiddleware.checkRole(['ADMINISTRADOR']), reportesController.createExcel);
+router.get('/pdf', authMiddleware.verifyToken, authMiddleware.checkRole(['ADMINISTRADOR']), reportesController.createPDF);
 
-router.get('/dashboard', reportesController.getReporteGerencial); //gerenciales 1
-router.get('/ingresos-mensuales', reportesController.getIngresosMensuales);//gerenciales 2
-router.get('/distribucion-facultades', reportesController.getDistribucionFacultades);//gerenciales 3
+router.get('/dashboard', authMiddleware.verifyToken, authMiddleware.checkRole(['ADMINISTRADOR']), reportesController.getReporteGerencial); //gerenciales 1
+router.get('/ingresos-mensuales', authMiddleware.verifyToken, authMiddleware.checkRole(['ADMINISTRADOR']), reportesController.getIngresosMensuales);//gerenciales 2
+router.get('/distribucion-facultades', authMiddleware.verifyToken, authMiddleware.checkRole(['ADMINISTRADOR']), reportesController.getDistribucionFacultades);//gerenciales 3
 
 module.exports = router;
