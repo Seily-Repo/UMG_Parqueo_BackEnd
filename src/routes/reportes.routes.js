94fce596e98c1285f9326516b7ce77f838dea8c7
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2026-05-20 19:37:46 -0600

    Update reportes.routes.js

--- src/routes/reportes.routes.js
+++ src/routes/reportes.routes.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const reportesController = require('../controllers/reportes.controller');
 
 //financieros
-router.get('reporte-financiero', reportesController.getReporteFinanciero);
-router.get('pagos-aceptados', reportesController.getPagosAceptados);
+router.get('/reporte-financiero', reportesController.getReporteFinanciero);
+router.get('/pagos-aceptados', reportesController.getPagosAceptados);
 
 //administrativos
-router.get('reporte-administrativo', reportesController.getReporteAdministrativo);
-router.get('excel', reportesController.createExcel);
-router.get('pdf', reportesController.createPDF);
+router.get('/reporte-administrativo', reportesController.getReporteAdministrativo);
+router.get('/excel', reportesController.createExcel);
+router.get('/pdf', reportesController.createPDF);
 
-router.get('dashboard', reportesController.getReporteGerencial); //gerenciales 1
-router.get('ingresos-mensuales', reportesController.getIngresosMensuales);//gerenciales 2
-router.get('distribucion-facultades', reportesController.getDistribucionFacultades);//gerenciales 3
+router.get('/dashboard', reportesController.getReporteGerencial); //gerenciales 1
+router.get('/ingresos-mensuales', reportesController.getIngresosMensuales);//gerenciales 2
+router.get('/distribucion-facultades', reportesController.getDistribucionFacultades);//gerenciales 3
 
 module.exports = router;
